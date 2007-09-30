06d67a36625c0f19dac88aa87261aae428c8c87d
tomasz.szymczyszyn <tomasz.szymczyszyn@localhost>
2007-09-30 20:54:35 +0000

    Trajectories generated by CircularMover and ZigZagMover are now smoother

--- agrajag/mover.py
+++ agrajag/mover.py
@@ -2,7 +2,8 @@
 #coding: utf-8
 
 """
-Module contain definitions of different types of movers (objects thay control movement of other objects).
+Module contain definitions of different types of movers (objects thay control
+movement of other objects).
 """
 
 import pygame
@@ -22,13 +23,18 @@ class Mover:
 
   def __init__(self):
     """
-    Crate mover instance and initialize its clock. Should not be used as this class is designed as abstract class and does not provide useful functionality on its own.
+    Crate mover instance and initialize its clock. Should not be used as this
+    class is designed as abstract class and does not provide useful
+    functionality on its own.
     """
 
     self.clock = 0
 
   def update(self):
-    """Return updated object's coordinates in a tuple. Needs to be overriden by child classes."""
+    """
+    Return updated object's coordinates in a tuple. Needs to be overriden
+    by child classes.
+    """
  
     self.clock += 1
     return 0, 0
@@ -36,7 +42,8 @@ class Mover:
 
 class RandomMover(Mover):
   """
-  Randomly changes direction of an object but not its speed. May allow object to float out of the viewport.
+  Randomly changes direction of an object but not its speed. May allow object
+  to float out of the viewport.
   
   @type pos: tuple
   @ivar pos: object's current position
@@ -84,45 +91,68 @@ class RandomMover(Mover):
 
 class ZigZagMover(Mover):
   """
-  Couses object to make zigzaging movement from top to the bottom. Exact trajectory is determined by objects C{speed} and
-  zigzag C{raius}.
+  Couses object to make zigzaging movement from top to the bottom. Exact
+  trajectory is determined by objects C{speed} and zigzag C{radius}.
+
+  @type init_pos: array
+  @ivar init_pos: object's initial position
 
   @type speed: integer
-  @ivar speed: object's speed
+  @ivar speed: object's linear speed in pixels per iteration
 
   @type radius: integer
   @ivar radius: zigzag radius
 
   @type period: float
-  @ivar period: time needed for one segment
+  @ivar period: time needed for one zigzag segment
+
+  @type ang_speed: float
+  @ivar ang_speed: object's angular speed measured in radians per iteration
   """
 
   def __init__(self, pos, speed, radius):
+    """
+    Create mover instance.
+
+    @type pos: array or tuple
+    @param pos: object's current position
+
+    @type speed: integer
+    @param speed: object's linear speed in pixels per iteration
+
+    @type radius: integer
+    @param radius: zigzag radius
+    """
+
     Mover.__init__(self)
 
-    self.init_pos = pos
+    self.init_pos = [pos[0], pos[1]]
     self.speed = speed
     self.radius = radius
-    self.period = math.pi * radius / float(self.speed)
+    self.period = math.pi * radius / float(speed)
+    self.ang_speed = self.speed / float(radius)
 
   def update(self):
     self.clock += 1
 
-    k = self.clock / int(self.period)
-    y = 2 * self.clock * self.radius / float(self.period)
-    x = math.sqrt(math.pow(self.radius, 2) - math.pow(y % (2 * self.radius) - self.radius, 2))
+    k = math.floor(self.clock / self.period)
+    x = self.radius * math.sin(self.ang_speed * self.clock) 
+    y = self.radius * math.cos(self.ang_speed * self.clock % math.pi + math.pi)
 
-    if k % 2 != 0:
-      x *= -1
-
+    y += (2 * k + 1) * self.radius
+    
     self.pos = self.init_pos[0] + x, self.init_pos[1] + y
     return self.pos
 
 
 class CircularMover(Mover):
   """
-  Causes object to make circular movement around its initial position. Before starting circular movement object moves in
-  random direction to enter its trajectory.
+  Causes object to make circular movement around its initial position. Before
+  starting circular movement object moves in random direction to enter its
+  trajectory.
+
+  @type init_pos: array
+  @ivar init_pos: object's initial position
 
   @type speed: integer
   @ivar speed: object's linear speed on circular trajectory
@@ -141,6 +171,7 @@ class CircularMover(Mover):
 
   @type init_dir: integer
   @ivar init_dir: randomly chosen direction used to enter circular trajectory
+  expressed in radians
   """
 
   def __init__(self, pos, speed, radius = 20):
@@ -156,21 +187,23 @@ class CircularMover(Mover):
 
     Mover.__init__(self)
 
-    self.pos = pos
+    self.init_pos = [pos[0], pos[1]]
     self.speed = speed
     self.radius = radius
-    self.ang_speed = rad2deg(speed / float(radius))
+    self.ang_speed = speed / float(radius)
     self.init_speed = speed / float(math.ceil(speed / float(radius)))
     self.init_time = radius / float(self.init_speed)
-    self.init_dir = random.randint(0, 359)
+    self.init_dir = 2 * math.pi * random.random()
 
   def update(self):
     self.clock += 1
     if self.clock <= self.init_time:
-      self.pos[0] += round(self.init_speed * math.sin(deg2rad(self.init_dir)))
-      self.pos[1] += round(self.init_speed * math.cos(deg2rad(self.init_dir)))
+      x = self.clock * self.init_speed * math.sin(self.init_dir)
+      y = self.clock * self.init_speed * math.cos(self.init_dir)
     else:
-      self.pos[0] += round(self.init_speed * math.sin(deg2rad(self.init_dir + 90 + self.ang_speed * (self.clock - self.init_time + 1))))
-      self.pos[1] += round(self.init_speed * math.cos(deg2rad(self.init_dir + 90 + self.ang_speed * (self.clock - self.init_time + 1))))
-
+      a = (self.clock - self.init_time) * self.ang_speed + self.init_dir
+      x = self.radius * math.sin(a)
+      y = self.radius * math.cos(a)
+    
+    self.pos = self.init_pos[0] + x, self.init_pos[1] + y
     return self.pos
